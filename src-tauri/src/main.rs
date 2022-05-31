4eb28f230eb0af24f4f7778296a83caa0fb8b147
cthies <carolinethies01@gmail.com>
2022-05-31 15:59:34 +0200

    emit and catch event when item is clicked

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,22 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::Manager;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      // listen to the `event-name` (emitted on any window)
+      let id = app.listen_global("select-litItem", |event| {
+        println!("got event-name with payload {:?}", event.payload());
+      });
+      // unlisten to the event using the `id` returned on the `listen_global` function
+      // an `once_global` API is also exposed on the `App` struct
+      app.unlisten(id);
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/components/LitCard.tsx
+++ src/components/LitCard.tsx
@@ -1,3 +1,6 @@
+import { emit } from "@tauri-apps/api/event";
+import { useState } from "react";
+
 export interface litCardProps {
   contributors: String[];
   title: String;
@@ -7,17 +10,25 @@ export interface litCardProps {
 }
 
 const LitCard = (props: litCardProps) => {
+  const [text, setText] = useState("");
   const contributor_string = props.contributors.join(", ");
 
   const id = "LitItem" + props.itemId;
 
+  let selectCard = () => {
+    if (window.__TAURI_METADATA__) {
+      emit("select-litItem", { itemId: props.itemId });
+    }
+  };
+
   return (
-    <div className="flexcol alignleft" id={id}>
+    <div className="flexcol alignleft" id={id} onClick={selectCard}>
       <div className="small">{contributor_string}</div>
       <div className="bold">{props.title}</div>
       <div className="small">
         {props.year} - {props.type}
       </div>
+      <div>{text}</div>
     </div>
   );
 };
